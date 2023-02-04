e4faedd56313667d223999a7ba18133bcd4faa5a
Marius <lesserknownthings@gmail.com>
2023-02-03 22:06:07 -0500

    Adding the input system
    -Created input actions
    -Created the World Game Object which manages the Input and UI (it's a singleton and needs to exits in every level)

---
+++ GameJamGame_Unity/Assets/Scripts/InputManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public delegate void OnInputActionTriggeredDelegate(InputActionType actionType, bool bIsClicked);
+
+public enum InputActionType
+{
+    None,
+    LMC,
+    RMC,
+    MMC,
+    Escape,
+    Action
+}
+
+public class InputManager : MonoBehaviour
+{
+    public OnInputActionTriggeredDelegate OnInputTriggered;
+
+    public Vector2 movementVector { get;private set; }
+    public Vector2 mousePosition { get; private set; }
+
+    private void OnMove(InputValue value)
+    {
+        movementVector = value.Get<Vector2>();
+    }
+
+    private void OnMousePosition(InputValue value)
+    {
+        mousePosition = value.Get<Vector2>();
+    }    
+
+    private void OnLMC(InputValue value)
+    {
+        OnInputTriggered?.Invoke(InputActionType.LMC, value.isPressed);
+    }
+
+    private void OnRMC(InputValue value)
+    {
+        OnInputTriggered?.Invoke(InputActionType.RMC, value.isPressed);
+    }
+
+    private void OnMMC(InputValue value)
+    {
+        OnInputTriggered?.Invoke(InputActionType.MMC, value.isPressed);
+    }
+
+    private void OnEscape(InputValue value)
+    {
+        OnInputTriggered?.Invoke(InputActionType.Escape, value.isPressed);
+    }
+
+    private void OnAction(InputValue value)
+    {
+        OnInputTriggered?.Invoke(InputActionType.Action, value.isPressed);
+        Helper.InternalDebugLog("Called Action");
+    }
+}
---
+++ GameJamGame_Unity/Assets/Scripts/World.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    #region Singleton
+    public static World Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                GameObject go = new GameObject("World");
+                return go.AddComponent<World>();
+            }
+            else
+            {
+                return instance;
+            }
+        }
+    }
+    private static World instance;
+    #endregion
+
+    [SerializeField]
+    private UIManager uiManager;
+    private UIManager uiManagerInternal;
+
+    [SerializeField]
+    private InputManager inputManager;
+    private InputManager inputManagerInternal;
+    
+    public UIManager GetUIManager()
+    {
+        if(uiManagerInternal == null)
+        {
+            if (uiManager != null)
+            {
+                uiManagerInternal = Instantiate(uiManager);
+            }
+            //This is gonna create an empty UIManager, you don't want this most likely
+            else
+            {
+                GameObject go = new GameObject("UI Manager");
+                uiManagerInternal = go.AddComponent<UIManager>();
+            }
+        }
+
+        return uiManager;
+    }
+
+    public InputManager GetInputManager()
+    {
+        if(inputManagerInternal == null)
+        {
+            if(inputManager != null)
+            {
+                inputManagerInternal = Instantiate(inputManager);
+            }
+            else
+            {
+                Helper.InternalDebugLog("The input manager is empty, please add an input manager", DebugType.Error);
+            }
+        }
+
+        return inputManagerInternal;
+    }
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        //Intializing the UI
+        GetUIManager();
+    }
+}
