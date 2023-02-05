a8d0ec0d1a9ef1785fecab0bc608ce3e47090012
sleduc <sleduc1995@gmail.com>
2023-02-05 13:26:10 -0500

    Removed camera bounds

--- GameJamGame_Unity/Assets/Scripts/CameraController.cs
+++ GameJamGame_Unity/Assets/Scripts/CameraController.cs
@@ -5,21 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public float smoothing;
     public Vector2 minPosition;
     public Vector2 maxPosition;
 
     void Update()
     {
-        if (transform.position != target.position)
-        {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-
-            targetPosition.x = Mathf.Clamp(target.position.x, minPosition.x, maxPosition.x);
-            targetPosition.y = Mathf.Clamp(target.position.y, minPosition.y, maxPosition.y);
-
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
-        }
-        
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 }
