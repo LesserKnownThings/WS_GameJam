92b1ed54b0565252205f6f36e2730d654704bd84
Marius <lesserknownthings@gmail.com>
2023-02-05 16:36:37 -0500

    Auto stash before merge of "main" and "origin/main"

--- GameJamGame_Unity/Assets/Scripts/PlayerMovementComponent.cs
+++ GameJamGame_Unity/Assets/Scripts/PlayerMovementComponent.cs
@@ -17,6 +17,8 @@ public class PlayerMovementComponent : MonoBehaviour
 
     private InputManager _inputManager;
 
+    [SerializeField] private Animator _animator;
+    
     private void Start()
     {
         _inputManager = World.Instance.GetInputManager();
@@ -28,9 +30,27 @@ public class PlayerMovementComponent : MonoBehaviour
         movementVector = _inputManager.movementVector;
     }
 
+    private string animationValName = "AnimationVal";
     private void FixedUpdate()
     {
-        _rigidbody2D.MovePosition( (Vector2)transform.position + _speed * Time.fixedDeltaTime * movementVector );
+        Vector2 position = transform.position;
+        _rigidbody2D.MovePosition( position + _speed * Time.fixedDeltaTime * movementVector );
+
+        if (movementVector.x == 0 && movementVector.y == 0)
+        {
+           // _animator.SetInteger(animationValName , -1);
+            _animator.speed = 0;
+        }
+        else if (Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y))
+        {
+            _animator.speed = 1;
+            _animator.SetInteger(animationValName, movementVector.x > 0 ? 0 : 1);
+        }
+        else
+        {
+            _animator.speed = 1;
+            _animator.SetInteger(animationValName, movementVector.y > 0 ? 2 : 3);
+        }
 
     }
 
